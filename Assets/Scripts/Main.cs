69e96029894514fa5c4639b8193bcb18327bb3df
josh giesbrecht <josh@thoughtlost.org>
2016-07-07 17:04:01 -0700

    flexible button array

--- Assets/Scripts/Main.cs
+++ Assets/Scripts/Main.cs
@@ -1,40 +1,43 @@
 ﻿using UnityEngine;
 using System.Collections;
 using UnityEngine.UI;
+using UnityEngine.EventSystems;
 using Ink.Runtime;
 
 public class Main : MonoBehaviour {
 
+	public VerticalLayoutGroup contentVlg;
+	public LayoutGroup choiceLg;
+	public ScrollRect scrollRect;
 	public Text maintext;
 
-	public Text choice1;
-	public Text choice2;
-	public Text choice3;
-	public Text choice4;
 
-	public Button b1;
-	public Button b2;
-	public Button b3;
-	public Button b4;
+	// Prefabs
+//	public GameObject storychunkPrefab;
+	public Button buttonPrefab;
 
 	public InkScript inkScript;
 
+	// tweakable thingies
 	public int textDelay;
 
+	// private variables
 	private string nextLine;
 	private bool waiting;
 	private int delay;
 	private bool typing;
-	private int lineCount;
+	private GameObject nextStoryChunk;
 
 	// Use this for initialization
 	void Start () {
 		nextLine = "";
-		maintext.text = "";
 		waiting = false;
 		delay = 0;
 		typing = false;
-		lineCount = 0;
+		textDelay = 1;
+
+//		nextStoryChunk = Instantiate (storychunkPrefab);
+//		nextStoryChunk.transform.SetParent (contentVlg.transform, false);
 	}
 
 	void Update() {
@@ -44,62 +47,65 @@ public class Main : MonoBehaviour {
 				if (delay > textDelay) {
 					delay = 0;
 					if (nextLine.Length > 0) {
+//						nextStoryChunk.GetComponent<Text>().text = string.Concat (nextStoryChunk.GetComponent<Text>().text, nextLine[0]);
 						maintext.text = string.Concat (maintext.text, nextLine[0]);
 						nextLine = nextLine.Substring (1);
 					} else {
 						typing = false;
-						if (lineCount > 5) {
-							// trim the top line off hopefully
-							maintext.text = maintext.text.Substring (
-								maintext.text.IndexOf (System.Environment.NewLine));
-						}
-
 					}
 				}
 			} else if (inkScript.CanContinue ()) {
 				typing = true;
-				b1.interactable = false;
-				b2.interactable = false;
-				b3.interactable = false;
-				b4.interactable = false;
-				choice1.text = "";
-				choice2.text = "";
-				choice3.text = "";
-				choice4.text = "";
+				RemoveChoices ();
+
 				nextLine = inkScript.Continue ();
+//				nextStoryChunk = Instantiate (storychunkPrefab);
+//				nextStoryChunk.transform.SetParent (contentVlg.transform, false);
+//
+//
+//				RectTransform cv = (RectTransform)contentVlg.transform;
+//				RectTransform st = (RectTransform)nextStoryChunk.transform;
+//				Vector2 tmp = new Vector2(cv.sizeDelta.x ,cv.sizeDelta.y + st.sizeDelta.y + 10);
+//				cv.sizeDelta = tmp;
+
 				maintext.text = string.Concat (maintext.text, System.Environment.NewLine);
-				lineCount++;
 
 			} else {
-				// check for choices?
+				// check for choices!
 				int numChoices = inkScript.choiceCount();
 				if (numChoices > 0) {
-					b1.interactable = true;
+					for (int i = 0; i < numChoices; i++) {
+						Debug.Log ("adding button");
+						UnityEngine.UI.Button nextButton = Instantiate (buttonPrefab);
+						nextButton.transform.SetParent (choiceLg.transform, false);
+						Text choiceText = nextButton.GetComponentInChildren<Text> ();
+						choiceText.text = inkScript.getChoice (i);
+
+						int tmp = i;
+						nextButton.onClick.AddListener (delegate {
+							makeChoice (tmp);
+						});
+
+					}
 					waiting = true;
-					choice1.text = inkScript.getChoice (0);
-				}
-				if (numChoices > 1) {
-					b2.interactable = true;
-					choice2.text = inkScript.getChoice (1);
-				}
-				if (numChoices > 2) {
-					b3.interactable = true;
-					choice3.text = inkScript.getChoice (2);
-				}
-				if (numChoices > 3) {
-					b4.interactable = true;
-					choice4.text = inkScript.getChoice (3);
-				}
-				if (numChoices > 4) {
-					Debug.Log ("WE NEED MOAR BUTTONS");
+				} else {
+					// I guess we're at the end of the story??!??!?!
 				}
 			}
 		}
 	}
 
 	public void makeChoice(int i) {
+		Debug.Log ("hi there " + i);
 		inkScript.chooseChoice (i);
 		waiting = false;
+	}
+
+	void RemoveChoices () {
+		int childCount = choiceLg.transform.childCount;
+		for (int i = childCount - 1; i >= 0; --i) {
+			GameObject.Destroy (choiceLg.transform.GetChild (i).gameObject);
+		}
 	}
 
 }
